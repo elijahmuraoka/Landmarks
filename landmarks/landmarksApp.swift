2269c8bcbdeb540fdc07823d9300ee0191e743bf
= <elijahmuraoka03@gmail.com>
2023-10-25 11:41:35 -0400

    Initial Commit

---
+++ landmarks/landmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmarksApp.swift
+//  landmarks
+//
+//  Created by Elijah Muraoka on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct landmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
